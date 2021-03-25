6477c52d46bcedbf26c57291122e2c4f81f5f1fa
Carlos Andrés Linares Benitez <carlosandreslinares01@gmail.com>
2021-03-24 23:42:10 -0500

    Importante

---
+++ Laboratorios/Lab 2303.py
@@ -0,0 +1,9 @@
+#Ejercicio 1
+for i in range (5):
+    for j in range (6):
+        print("i vale ", i, "y j vale", j)
+#Ejercicio 2
+print("Ejercicio 2")
+for i in range (5):
+    for j in range (i):
+        print("i vale ", i, "y j vale", j)
