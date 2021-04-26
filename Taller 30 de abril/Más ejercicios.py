d2bc3d47618cc5829b0031b7698f66c3e150d5ca
Carlos Andrés Linares Benitez <carlosandreslinares01@gmail.com>
2021-04-26 17:28:00 -0500

    Taller 30 de abril

--- Taller 30 de abril/Más ejercicios.py
+++ Taller 30 de abril/Más ejercicios.py
@@ -1,8 +1,10 @@
 #Ejercicio 7 - Programa que imprima el area y el perimetro de un circulo
-r = 5
-areac = 3,14 * r ** 2
-perimetro = 2 * 3,14 * r
-print("El area del circulo es: ", areac, "y el perimetro es: ", perimetro)
+radio = float(input("Ingrese el radio del cirulo: "))
+pi = 3.14
+areac = pi * (radio ** 2)
+perimetro = (2 * pi) * radio
+print("El area del circulo es: ", areac)
+print("El perimetro del circulo es: ", perimetro)
 #Ejercicio 6 - Programa que muestre la venta de un producto con y sin IVA
 pan = 2000
 IVA = (pan*19/100)
@@ -10,11 +12,11 @@ paniva = (pan+IVA)
 print("El costo original del pan es: ", + pan, ",el IVA es de: ", + IVA, "Y el costo del pan con IVA es de: ", +paniva)
 #Ejercicio 27 - Programa que pida dos numeros y determine el mayor
 s = int(input("Ingrese un numero: "))
-z = int(input("Ingrese un numero: "))
-if s>z:
-    print(s, "Es mayor que ", + z)
+y = int(input("Ingrese un numero: "))
+if s>y:
+    print(s, "Es mayor que ", + y)
 else:
-    print(z, "Es mayor que ", + s)
+    print(y, "Es mayor que ", + s)
 
 #Ejercicio 28 - Programa que convierta un numero a decimal
 p = int(input("Ingrese un numero: "))
@@ -24,19 +26,21 @@ print("El decimal del numero es = ", + deci)
 #Ejercicio 29 - Programa que pida tres numeros y determine el mayor y el menor
 t = int(input("Ingrese un numero: "))
 x = int(input("Ingrese un numero: "))
-y = int(input("Ingrese un numero: "))
-if t>x:
-    if t>y:
-        print("El numero mayor es: ", + t)
-elif x>t and x>y:
-    print("El numero mayor es: ", + z)
+z = int(input("Ingrese un numero: "))
+if (t>x) and (t>z) and (x>z):
+    print("El numero mayor es: ", + t, "Y el numero menor es: ", z)
 else:
-    print("El numero mayor es ", + y)
-
-if t<x:
-    if t<y:
-        print("El numero menor es: ", + t)
-elif z<t and z<t:
-    print("El numero menor es: ", + z)
-else:
-    print("El numero menor es: ", + y)
+    if (t>x) and (t>z) and (z>x):
+        print("El numero mayor es: ", + t, "Y el numero menor es: ", x)
+    else:
+        if (z>t) and (z>x) and (t>x):
+            print("El numero mayor es: ", + z, "Y el numero menor es: ", x)
+        else:
+            if (z>t) and (z>x) and (x>t):
+                print("El numero mayor es: ", + z, "Y el numero menor es: ", t)
+            else:
+                if (x>t) and (x>z) and (z>t):
+                    print("El numero mayor es: ", + x, "Y el numero menor es: ", t)
+                else:
+                    if (x>t) and (x>z) and (t>z):
+                        print("El numero mayor es: ", + x, "Y el numero menor es: ", z)
